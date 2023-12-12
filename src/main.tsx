bf5547266c557e21767ee2e004bf174102a8e2de
andr-33 <andree.serranourraca@gmail.com>
2023-12-12 16:23:05 +0100

    fix: dotenv

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
